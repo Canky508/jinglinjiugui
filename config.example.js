89ea8f979256e98862f37c6b5a41ca1bbf7e6bbe
Canky508 <156871256+Canky508@users.noreply.github.com>
2026-06-06 23:31:07 +0800

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,3 @@
+// 部署前复制为 config.js，填入线上 API 地址（不要末尾斜杠）
+// 例：window.JINLING_API = 'https://jinling-api.onrender.com';
+window.JINLING_API = '';
---
+++ config.js
@@ -0,0 +1,2 @@
+// 本地开发留空；部署到公网时改成你的 API 地址
+window.JINLING_API = '';
